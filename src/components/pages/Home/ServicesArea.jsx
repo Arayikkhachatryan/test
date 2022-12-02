336db426f0d5d7019fa1a0d8b3c3a6adf6dc95d0
arayikkhachatryan <solnce223@gmai.com>
2022-12-02 16:15:01 +0400

    added our services in home page

--- src/components/pages/Home/ServicesArea.jsx
+++ src/components/pages/Home/ServicesArea.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
 
 const ServicesArea = () => {
   return (
     <section className="services-section">
       <div className="container">
         <div className="services-section-wrapper">
-          <div className="services-section-cards">
-            <div className="services-section-single-card"></div>
-          </div>
-
           <div className="services-section-text">
             <h2>Our Services</h2>
             <p>
@@ -17,6 +14,68 @@ const ServicesArea = () => {
               velit voluptates perspiciatis tempore! Odio, reprehenderit veniam
               quibusdam non repellat illo fugit?
             </p>
+          </div>
+          <div className="services-section-cards">
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
+            <div className="services-section-single-card">
+              <AiOutlineUser />
+              <div className="services-section-card-content">
+                <h3>Some Service</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Aperiam ullam exercitationem laborum amet quos quo voluptatem.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
